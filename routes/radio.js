eacd46d82046c2a60980f5ea1ea42b4082a90381
pamukh294-ctrl <pamukh294@gmail.com>
2026-05-11 12:06:05 +0300

    final working radio player fix

--- routes/radio.js
+++ routes/radio.js
@@ -8,12 +8,12 @@ router.get("/", (req, res) => {
       url: "https://stream-uk1.radioparadise.com/mp3-192"
     },
     {
-      name: "BBC Radio 1",
-      url: "http://stream.live.vc.bbcmedia.co.uk/bbc_radio_one"
-    },
-    {
       name: "KEXP",
       url: "https://kexp-mp3-128.streamguys1.com/kexp128.mp3"
+    },
+    {
+      name: "BBC Radio 1",
+      url: "http://stream.live.vc.bbcmedia.co.uk/bbc_radio_one"
     }
   ]);
 });
